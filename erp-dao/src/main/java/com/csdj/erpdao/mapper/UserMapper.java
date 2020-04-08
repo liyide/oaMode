9f771385f35f341314ca3ef5784cbe2d6c7c0f05
lyd <li_yide@163.com>
2020-04-08 16:01:40 +0800

    注释修改

---
+++ erp-dao/src/main/java/com/csdj/erpdao/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.csdj.erpdao.mapper;
+
+import com.csdj.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 修改一次注释
+ */
+@Mapper
+public interface UserMapper extends tk.mybatis.mapper.common.Mapper<User>{
+}
